66941faa3952b734382672c3f2a58995d8eeb58a
Hong <hc103081@gamil.com>
2025-10-04 21:58:58 +0800

    test

--- program/test.py
+++ program/test.py
@@ -1,9 +1,20 @@
 from line_bot import Line_bot
+import RPi.GPIO as GPIO    
+from gpio import GPIO
 
-line_bot = Line_bot()
-id = 'U44a5e3e3cf9c8835a64bb1273b08f457'
-for i in range(5):
-    line_bot.sent_message(id,'gay')
+
+def line_test():
+    line_bot = Line_bot()
+    id = 'U44a5e3e3cf9c8835a64bb1273b08f457'
+    for i in range(5):
+        line_bot.sent_message(id,'gay')
+        
+def gpio_test():
+    print("")
+    
+
+if __name__ == "__main__":
+    gpio_test()
 
 
 # 设置判断参数
