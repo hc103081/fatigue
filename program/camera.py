9116769d397d11bcb0dbb8cdfbbc78b246b05002
Hong <hc103081@gamil.com>
2025-10-07 23:24:35 +0800

    with

--- program/camera.py
+++ program/camera.py
@@ -13,11 +13,11 @@ class Camera:
         try:
             self.cap = cv2.VideoCapture(camera_index)  # 初始化攝像頭
             
-        except AttributeError:
-            raise RuntimeError("無法初始化攝像頭，請檢查攝像頭是否連接正確")
+        except cv2.error:
+            raise Exception("無法初始化攝像頭")
         
         
-    # 讀取一幀影像
+     # 讀取一幀影像
     def get_frame(self):
         """ 
         讀取並回傳一幀影像\n
@@ -27,9 +27,25 @@ class Camera:
         if not ret:
             return None
         return frame
-
+    
     # 關閉攝像頭
     def close(self):
         """ 關閉攝像頭 """
+        if self.cap.isOpened():
+            self.cap.release()
+            cv2.destroyAllWindows()
+    
+    def __enter__(self):
+        """ 支持 with 語句 """
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """ 退出 with 語句時自動關閉攝像頭 """
+        self.close()
+        if exc_type is not None:
+            print(exc_type, exc_val, exc_tb)
+            return False  # 攔截例外並回傳 False
+        return True  # 正常結束 with 語句
+    
         
         
