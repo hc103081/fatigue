037d87f315cff91869ff763997813e7cb71251d2
Hong <hc103081@gmail.com>
2025-10-13 14:08:23 +0800

    test cameta3

--- program/test.py
+++ program/test.py
@@ -68,7 +68,7 @@ def camera_test2():
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
+ 
     while True:
         ret, frame = cap.read()
         if not ret:
