e8b3911e7273f22024be4be6a0bb544d17658616
Hong <hc103081@gmail.com>
2025-11-03 18:33:46 +0800

    web10

--- program/web_Api.py
+++ program/web_Api.py
@@ -66,7 +66,7 @@ class WebApi():
             img_base64 = base64.b64encode(buffer).decode('utf-8')
             if self.connected:
                 try:
-                    self.sio.emit('image_update', {"image": img_base64})
+                    self.sio.emit('image_update', img_base64)
                 except Exception as e:
                     Log.logger.warning(f"send_image failed: {e}")
             else:
