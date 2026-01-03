07c2a9e8abb35f643ea5e5bfa48b48f0e6007ab4
Hong <hc103081@gmail.com>
2026-01-03 22:45:34 +0800

    merge1

--- program/__init__.py
+++ program/__init__.py
@@ -1,13 +1,10 @@
 from .dataClass import DataUnified,ClassUnified
 
 # program class
-from .heart import HeartRateSensor
 from .alcohol import AlcoholSensor
 from .face_analyze import FaceAnalyzer
 from .line_Api import Line_Api
 from .line_bot import Line_bot
-from .ngrok import Ngrok
-from .web_Api import WebApi
 from .gpio import GPIO
 from .logs import Log
 from .camera import Camera
@@ -18,13 +15,10 @@ from .mp3_player import MP3Player
 __all__ = [
     "DataUnified",
     "ClassUnified",
-    "HeartRateSensor",
     "AlcoholSensor",
     "FaceAnalyzer",
     "Line_Api",
     "Line_bot",
-    "Ngrok",
-    "WebApi",
     "GPIO",
     "Log",
     "Camera",
